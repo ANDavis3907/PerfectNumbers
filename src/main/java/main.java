1a5cf20dab94da48685586ab9a26660f75394f83
Alexis <ANDavis3907@gmail.com>
2024-12-22 22:19:31 +0000

    ide set up

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(string[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Hello, " + name + "!");
+        input.close();
+    }
+}
